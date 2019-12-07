a5a590962f211c2c1e7b497a37f8e098d5b4d0df
Daryna Oliinyk <darusya172@gmail.com>
2019-12-07 18:08:33 +0200

    some changes in ShowRequestsViewModel

--- Tester/ViewModels/ShowRequestsViewModel.cs
+++ Tester/ViewModels/ShowRequestsViewModel.cs
@@ -1,7 +1,35 @@
-﻿
+﻿using System;
+using Tester.Managers;
+using Tester.Tools;
+using Tester.Tools.Navigation;
+
 namespace Tester.ViewModels
 {
     class ShowRequestsViewModel
     {
+
+        #region Fields
+
+        private RelayCommand<object> _newRequestCommand;
+
+        #endregion
+
+        #region Properties
+        public RelayCommand<Object> NewRequestCommand
+        {
+            get
+            {
+                return _newRequestCommand ?? (_newRequestCommand =
+                           new RelayCommand<object>(NewRequestImplementation));
+            }
+        }
+
+        #endregion
+
+        private void NewRequestImplementation(object obj)
+        {
+            //NavigationManager.Instance.Navigate(ViewType.);
+        }
+
     }
 }
